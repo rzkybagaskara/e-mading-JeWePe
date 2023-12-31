da22cec37396e34b2d26c1e7d9f38f9e1f206cfc
Rizky Bagaskara <rizky.6abu@hotmail.com>
2023-12-31 14:06:18 +0700

    tambah detail artikel

--- detail.php
+++ detail.php
@@ -3,9 +3,29 @@
     include "dashboard/templates/header.php";
 
     // tampilkan semua hal yang berkaitan dengan artikel
-    // ambil artikel berdasarkan id artikelnya
+    // ambil artikel berdasarkan id artikelnya dari URL (ternary operator)
+    $id_artikel = isset($_GET['id_artikel']) ? $_GET['id_artikel'] : null;
 
-
-    $sql = "SELECT * FROM artikel WHERE id_artikel = ''";
-    $result = mysqli_query($conn, $sql);
+    if($id_artikel){
+        $sql = "SELECT * FROM artikel WHERE id_artikel = '$id_artikel'";
+        $result = mysqli_query($conn, $sql);
+        $row = mysqli_fetch_assoc($result); //fetch associative array (or objects in JS)
+        
+        // akses nilainya
+        $judul = $row['judul'];
+        $isi = $row['isi'];
+        $gambar= $row['gambar'];
+        $uploader= $row['uploader'];
+    }else{
+        echo "Artikel tidak ditemukan";
+        exit();
+    }
 ?>
+
+<!-- Detail Artikel -->
+<div class="detail-artikel">
+    <h3 class="mt-1 ml-1"> <?php echo $judul;?> </h3>
+    <h5 class="mt-1 ml-1"> Uploader: <?php echo $uploader;?> </h5>
+    <img src="public/img/<?php echo $gambar; ?>" alt="Gambar Artikel" class="img-fluid">
+    <p class="mt-1 ml-1"> <?php echo $isi; ?> </p>
+</div>
